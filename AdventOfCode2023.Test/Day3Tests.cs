259a6983eb8ddf713a5f7efca644cabbe6ebdbcb
Jon Whitter <me@jonwhitter.com>
2023-12-03 09:16:06 +0000

    added day 3 solver

---
+++ AdventOfCode2023.Test/Day3Tests.cs
@@ -0,0 +1,41 @@
+namespace AdventOfCode2023.Test;
+
+using AdventOfCode2023.Solvers;
+
+public class Day3Tests
+{
+    private const string Data = @"467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+
+    [Fact]
+    public void Day3_Part1()
+    {
+        var solver = new Day3Solver();
+
+        var data = solver.ParseData(Data);
+
+        var result = solver.SolvePart1(data);
+
+        Assert.Equal(4361, result);
+    }
+
+    [Fact]
+    public void Day3_Part2()
+    {
+        var solver = new Day3Solver();
+
+        var data = solver.ParseData(Data);
+
+        var result = solver.SolvePart2(data);
+
+        Assert.Equal(467835, result);
+    }
+}
---
+++ AdventOfCode2023/Solvers/Day3Solver.cs
@@ -0,0 +1,53 @@
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023.Solvers;
+
+public record Part(int Number, (int y, int x)? Position);
+
+static partial class Extensions
+{    
+    public static IEnumerable<Part> ToParts(this string[] values)
+    {
+        for(var i = 0; i < values.Length; i++)
+        {
+            var matches = FindNumberRegex().Matches(values[i]).AsQueryable();
+            
+            foreach(var match in matches)
+            {
+                for(var y = i - 1; y <= i + 1; y++ )
+                {
+                    for(var x = match.Index - 1; x <= match.Index + match.Length; x ++)
+                    {
+                        if(y < 0 || y >= values.Length || x < 0 || x >= values[i].Length)
+                        {
+                            continue;
+                        }
+
+                        if(char.IsDigit(values[y][x]) || values[y][x] == '.')
+                        {
+                            continue;
+                        }
+
+                        yield return new Part(int.Parse(match.Value), values[y][x] == '*' ? (y, x) : null);
+                    }
+                }
+            }
+        }    
+    }
+
+    [GeneratedRegex(@"(\d+)")]
+    private static partial Regex FindNumberRegex();
+}
+
+public class Day3Solver : BaseSolver<IEnumerable<Part>, int>
+{
+    public override IEnumerable<Part> ParseData(string rawData) => rawData.SplitByNewline<string>().ToParts();    
+
+    public override int SolvePart1(IEnumerable<Part> inputData) => inputData.Sum(x => x.Number);
+
+    public override int SolvePart2(IEnumerable<Part> inputData) =>
+        inputData
+            .GroupBy(x => x.Position)
+            .Where(x => x.Count() == 2)
+            .Sum(x => x.First().Number * x.Last().Number);
+}
